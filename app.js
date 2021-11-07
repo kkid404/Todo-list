0810faecc298aa0aba4087280db461591090a961
kkid404 <vizzy1488@gmail.com>
2021-11-07 19:38:38 +0500

    Полностью законченный и рабочий туду-лист

---
+++ app.js
@@ -0,0 +1,66 @@
+let addMessage = document.querySelector('.message')
+let addButton = document.querySelector('.add')
+let todo = document.querySelector('.todo')
+
+let todoList = [];
+
+if(localStorage.getItem('todo')){
+    todoList = JSON.parse(localStorage.getItem('todo'))
+    displayMessage()
+}
+
+addButton.addEventListener('click', function(){
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    }
+
+    todoList.push(newTodo)
+    displayMessage()
+    localStorage.setItem('todo', JSON.stringify(todoList)) //обязательно загуглить
+    addMessage.value = ''
+})
+
+function displayMessage(){
+    let displayMessage = ''
+    if(todoList.length === 0){todo.innerHTML = ''} 
+    todoList.forEach(function(item, i){
+        displayMessage += `
+        <li>
+        
+        <input type="checkbox" id='item_${i}' ${item.checked ? 'checked' : ''}>
+        <label for="item_${i}" class="${item.important ? 'important': ''}">${item.todo}</label>
+        </li>   
+        `
+        todo.innerHTML = displayMessage
+    })
+}
+
+todo.addEventListener('change', function(event){
+    let idInput = event.target.getAttribute('id')
+    let forLabel = todo.querySelector('[for = ' + idInput + ']') 
+    let valueLabel = forLabel.innerHTML
+
+    todoList.forEach(function(item){
+        if(item.todo === valueLabel){
+            item.checked = !item.checked
+            localStorage.setItem('todo', JSON.stringify(todoList)) 
+        }
+    })
+})
+
+todo.addEventListener('contextmenu', function(event){
+    event.preventDefault()
+    todoList.forEach(function(item, i){
+        if(item.todo === event.target.innerHTML){
+            if(event.ctrlKey || event.metaKey){
+                todoList.splice(i, 1)
+            }else{
+                item.important = !item.important
+            }
+            displayMessage()
+            localStorage.setItem('todo', JSON.stringify(todoList)) 
+        }
+    })
+})
